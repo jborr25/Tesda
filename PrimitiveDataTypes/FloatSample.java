f17e14eae3bf3a80ab473cbb7dbbed31b8681f7f
jborr25 <jovetborrico@gmail.com>
2024-08-29 10:07:47 +0000

    update

---
+++ PrimitiveDataTypes/FloatSample.java
@@ -0,0 +1,10 @@
+package PrimitiveDataTypes;
+//single-precision 32-bit IEEE 754 floating point
+//used to save memory in large arrays of floating point numbers.
+public class FloatSample{
+    public static void main(String[] args){
+        Float F = 10.5f;
+        System.out.println("float value:" + F);
+    }
+
+}
---
+++ PrimitiveDataTypes/LongSample.java
@@ -0,0 +1,9 @@
+package PrimitiveDataTypes;
+//64-bit signed two's complement integer
+//minimum value of -2^63 and a maximum value of 2^63 - 1 (inclusive)
+public class LongSample {
+    public static void main(String[] args) {
+        long l = 100000L;
+        System.out.println("Long value: " + l);
+    }
+}

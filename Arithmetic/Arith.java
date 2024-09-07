e277d9830d87e60761d6db5b9db43fd15b4ba9a4
jborr25 <jovetborrico@gmail.com>
2024-09-07 12:43:12 +0000

    update

---
+++ Arithmetic/Arith.java
@@ -0,0 +1,17 @@
+package Arithmetic;
+
+public class Arith {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        int sum = a+b;
+        int difference = a-b;
+        int product = a*b;
+        int quotient = a/b;
+    System.out.println(sum);
+    System.out.println(difference);
+    System.out.println(product);
+    System.out.println(quotient);
+
+    }
+}
